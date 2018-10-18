8228f9574d4b3a5381aa291619b1a9e5d57313a1
Phelipe Alves de Souza <sougyn@gmail.com>
2018-10-18 19:13:12 -0300

    Adding setupt Manifest

--- setup.py
+++ setup.py
@@ -1,25 +1,44 @@
 import setuptools
+import re
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
+def load_reqs(filename):
+    with open(filename) as reqs_file:
+        return [
+            re.sub('==', '>=', line) for line in reqs_file.readlines()
+            if not re.match('\s*#', line)
+        ]
+
+
+requirements = load_reqs('requirements.txt')
+
 setuptools.setup(
-    name="sw4iot_etcd",
-    version="0.0.2",
+    name="sw4iot-etcd",
+    version="0.0.7",
     author="SOFTWAY4IoT",
-    author_email="sw4iot@gmail.com",
+    author_email="softway4iot@gmail.com",
     description="Etcd of SOFTWAY4IoT",
+    license="MIT License",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sw4iot/sw4iot-etcd",
-    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    packages=[
+        'sw4iot_etcd'
+    ],
+    package_dir={
+        'sw4iot_etcd': 'sw4iot_etcd'
+    },
+    keywords='sw4iot-etcd',
+    include_package_data=True,
+    zip_safe=False,
     classifiers=[
         'Programming Language :: Python :: 2.7',
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
-    install_requires=[
-        'etcd3'
     ]
 )
